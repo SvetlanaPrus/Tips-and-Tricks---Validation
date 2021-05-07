e8bfeffc49c168322c7d6292f35bbf056a83def0
Andrey Prisniak <aprisniak@lectrum.io>
2021-05-07 15:52:38 +0300

    feat(source): initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* Core */
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+
+/* Components */
+
+export const App = () => {
+    return (
+        <>
+            <h1>Добро пожаловать на профессиональный курс по Rect, Redux, MobX, TypeScript</h1>
+        </>
+    );
+};
